fdcb3d3123ea834f537bb7f7f14dd891dccd1e32
Righson <ya-fhh@yandex.ru>
2017-05-11 15:52:24 +0300

    card class
    CardThread open link and prepare data to use

---
+++ app/card.py
@@ -0,0 +1,28 @@
+from threading import Thread
+from bs4 import BeautifulSoup as Soup
+import urllib.request
+
+
+class CardThread(Thread):
+
+    url = 'http://begin-english.ru/word-rand'
+
+    def __init__(self):
+        self.data = None
+        Thread.__init__(self)
+
+    def run(self):
+        data = Soup(urllib.request.urlopen(self.url).read(), 'html.parser').find('div', {'class': 'word'})
+        self.data = {'word': str(data.find('div').text), 'description': self.setDescription(data.findAll('div')[2])}
+
+    def setDescription(self, in_obj):
+
+        string = ''
+
+        for el in in_obj:
+            if el.name == 'h2':
+                string += "\n%s\n" % el.string.upper()
+            elif el.name is not None:
+                string += "%s\n" % el.text
+
+        return string
---
+++ test/cardtest.py
@@ -0,0 +1,27 @@
+import unittest
+# from bs4 import BeautifulSoup as Soup
+from app.card import CardThread
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_openSite(self):
+        c = CardThread()
+        self.assertIsNone(c.data)
+
+    def test_getRandomDataFromSite(self):
+        threads = []
+        for ii in range(3):
+            threads.append(CardThread())
+            threads[ii].start()
+            threads[ii].join()
+
+        self.assertNotEqual(threads[0].data['word'], threads[1].data['word'])
+        self.assertNotEqual(threads[1].data['word'], threads[2].data['word'])
+        self.assertNotEqual(threads[2].data['word'], threads[0].data['word'])
+
+        self.assertIsNotNone(threads[0].data['word'])
+        self.assertIsNotNone(threads[1].data['word'])
+        self.assertIsNotNone(threads[2].data['word'])
+
+if __name__ == '__main__':
+    unittest.main()
